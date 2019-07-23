7bcd4ae6d41d3247be2db3c413c64a34032c063f
solitas <solitasroh@gmail.com>
2019-07-23 20:37:09 +0900

    Initialize commit

---
+++ Notify/ImageHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductivityTool.Notify
+{
+    class ImageHelper
+    {
+    }
+    #region FileToImageIconConverter
+    public static class FileToImageIconConverter
+    {
+
+        public static System.Windows.Media.ImageSource Icon(string filePath)
+        {
+            System.Windows.Media.ImageSource icon = null;
+            if (System.IO.File.Exists(filePath))
+            {
+                using (System.Drawing.Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(filePath))
+                {
+                    icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                              sysicon.Handle,
+                              System.Windows.Int32Rect.Empty,
+                              System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                }
+            }
+
+            return icon;
+
+        }
+        
+    }
+    #endregion
+}
